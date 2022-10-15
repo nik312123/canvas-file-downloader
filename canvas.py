f31fea131cf9b116892be8684108ed23be0d03b4
Nikunj Chawla <nikchawla312@gmail.com>
2022-10-14 19:28:36 -0500

    Formatting

--- canvas.py
+++ canvas.py
@@ -8,7 +8,7 @@ import re
 import colorama
 import requests
 
-colorama.init(autoreset=True)
+colorama.init(autoreset = True)
 
 
 def print_c(string, type_, padding, **kwarg):
@@ -35,14 +35,14 @@ def get_external_download_url(url: str) -> str:
     Supported sites:
     - docs.google.com
     """
-
+    
     # Try Google Drive
     exp = re.compile(r"https://drive\.google\.com/file/d/(?P<id>[^/]*?)/")
     result = exp.search(url)
     if result:
         document_id = result.group("id")
         return f"https://docs.google.com/uc?export=download&id={document_id}"
-
+    
     return ""
 
 
@@ -64,55 +64,55 @@ def get_file_name_by_header(header) -> str:
 @dataclasses.dataclass
 class CanvasApi:
     """Canvas REST API wrapper"""
-
+    
     # Check https://canvas.instructure.com/doc/api/
-
+    
     domain: str
     token: str
-
+    
     def __url(self, query):
         return "/".join(("https:/", self.domain, "api/v1", query))
-
+    
     def __get(self, query: str, **kwarg):
         response = requests.get(
-            url=self.__url(query),
-            headers={"Authorization": f"Bearer {self.token}"},
+            url = self.__url(query),
+            headers = {"Authorization": f"Bearer {self.token}"},
             **kwarg,
         )
         return response.json()
-
+    
     def get_courses(self, only_favorites: bool = True) -> list:
         """Returns the enrolled courses"""
         if only_favorites:
             return self.__get("users/self/favorites/courses")
         return self.__get("courses")
-
+    
     def get_folders(self, course_id: int) -> list:
         """Gets the folders of a course"""
         return self.__get(f"courses/{course_id}/folders")
-
+    
     def get_modules(self, course_id: int) -> list:
         """Gets the modules of a course"""
         return self.__get(f"courses/{course_id}/modules")
-
-    def get_files_from_folder(self, folder_id: int, recent=True) -> list:
+    
+    def get_files_from_folder(self, folder_id: int, recent = True) -> list:
         """Gets the files of a folder"""
         # TODO: the api by default gets only the first 10 uploaded files
         if recent:
             self.__get(
                 f"folders/{folder_id}/files",
-                params={"sort": "updated_at", "order": "desc"},
+                params = {"sort": "updated_at", "order": "desc"},
             )
         return self.__get(f"folders/{folder_id}/files")
-
+    
     def get_modules_items(self, course_id: int, module_id: int) -> list:
         """Gets the module items of a course"""
         return self.__get(f"courses/{course_id}/modules/{module_id}/items")
-
+    
     def get_file_from_id(self, course_id: int, file_id: int) -> dict:
         """Gets a file of a specific course using its id"""
         return self.__get(f"courses/{course_id}/files/{file_id}")
-
+    
     def get_folder_from_id(self, course_id: int, folder_id: int) -> dict:
         """Gets a folder from a specific course using its id"""
         return self.__get(f"courses/{course_id}/folders/{folder_id}")
@@ -121,82 +121,82 @@ class CanvasApi:
 @dataclasses.dataclass
 class CanvasDownloader(CanvasApi):
     """Canvas file downloader"""
-
+    
     out_dir: str
-
-    def download_files(self, all_courses=False, courses_ids=None, use="both"):
+    
+    def download_files(self, all_courses = False, courses_ids = None, use = "both"):
         """Downloads files from Canvas"""
         courses = self.get_courses(not all_courses)
-
+        
         if "errors" in courses:
             print_c("error: " + courses["errors"][0]["message"], "error", 0)
             return False
-
+        
         for course in courses:
             restricted = course.get('access_restricted_by_date')
             if restricted:
                 print(f"The course with ID {course['id']} was restricted by date: {course}")
                 continue
-            print_c(course["course_code"], type_="group", padding=0)
+            print_c(course["course_code"], type_ = "group", padding = 0)
             course_code, course_id = course["id"], course["course_code"]
-
+            
             methods = [self._download_from_modules, self._download_from_folders]
-
+            
             if use == "both":
                 for method in methods:
                     method(course_code, course_id)
                 continue
-
+            
             if use == "folders":
                 methods.reverse()
-
+            
             available = methods[0](course_code, course_id)
             if not available:
                 methods[1](course_code, course_id)
         return True
-
+    
     def _download_from_folders(self, course_id, course_name) -> bool:
         folders_list = self.get_folders(course_id)
         for folder in folders_list:
-
+            
             if not folder["files_count"]:
                 continue
-
+            
             files_list = self.get_files_from_folder(folder["id"])
-
+            
             if "errors" in files_list:
                 return False
-
+            
             folder_path = [course_name] + folder["full_name"].split("/")[1:]
             print_c("[F] " + folder["full_name"], "item", 1)
-
+            
             for file_obj in files_list:
                 if not file_obj["url"]:
                     continue
-
+                
                 self._download_file(
                     file_obj["url"], folder_path, file_obj["display_name"]
                 )
-
+        
         return True
-
+    
     def _download_from_modules(self, course_id, course_name) -> bool:
         modules_list = self.get_modules(course_id)
-
+        
         for module in modules_list:
-
+            
             if not module["items_count"]:
                 continue
-
+            
             module_items = self.get_modules_items(course_id, module["id"])
-
+            
             if "errors" in module_items:
                 return False
-
+            
             # TODO: A module can have a name that is not a valid path
             module_path = [course_name, module["name"].strip().replace("/", "&")]
             print_c("[M] " + module["name"], "item", 1)
-
+            
             for item in module_items:
                 if item["type"] == "File":
                     file_obj = self.get_file_from_id(course_id, item["content_id"])
@@ -212,36 +212,36 @@ class CanvasDownloader(CanvasApi):
                     download_url = get_external_download_url(item["external_url"])
                     if download_url:
                         self._download_file(download_url, module_path)
-
+        
         return True
-
-    def _download_file(self, file_url, folder_path, name= ""):
+    
+    def _download_file(self, file_url, folder_path, name = ""):
         """Downloads a file from its URL.
         If a file name is given, the download request won't happen
         if a file with the same name exists.
         """
         dir_path = os.path.join(self.out_dir, *folder_path)
-
+        
         # See if the directory is valid
         try:
-            os.makedirs(os.path.join(dir_path), exist_ok=True)
+            os.makedirs(os.path.join(dir_path), exist_ok = True)
         except NotADirectoryError:
-            print_c("error: invalid path", type_="error", padding=2)
+            print_c("error: invalid path", type_ = "error", padding = 2)
             return
-
+        
         if name:  # if a name in given
             # Check the file name
             file_name = name
             # Checks if the file exists
             file_path = os.path.join(self.out_dir, *folder_path, file_name)
             if os.path.exists(file_path):
-                print_c(file_name, type_="existing", padding=2)
+                print_c(file_name, type_ = "existing", padding = 2)
                 return
             # Starts the request if it doesn't
-            download_response = requests.get(file_url, stream=True)
+            download_response = requests.get(file_url, stream = True)
         else:
             # Starts the request
-            download_response = requests.get(file_url, stream=True)
+            download_response = requests.get(file_url, stream = True)
             content_header = download_response.headers.get("Content-Disposition")
             # Check the file name
             if not content_header:
@@ -252,56 +252,56 @@ class CanvasDownloader(CanvasApi):
             # Checks if the file exists
             file_path = os.path.join(self.out_dir, *folder_path, file_name)
             if os.path.exists(file_path):
-                print_c(file_name, type_="existing", padding=2)
+                print_c(file_name, type_ = "existing", padding = 2)
                 return
-
+        
         content_len = download_response.headers.get("content-length", None)
-
+        
         # Download file
-        print_c(" | ".join((f"{0:3.0f}%", file_name,)), "new", 2, end="\r")
+        print_c(" | ".join((f"{0:3.0f}%", file_name,)), "new", 2, end = "\r")
         with open(file_path, "wb") as file:
             if not content_len:
                 print_c(" | ".join(("???%", file_name,)), "new", 2)
                 file.write(download_response.content)
                 return
-
+            
             progress = 0
             total_len = int(content_len)
-
-            for data in download_response.iter_content(chunk_size=4096):
+            
+            for data in download_response.iter_content(chunk_size = 4096):
                 file.write(data)
                 progress += len(data)
                 perc = (progress / total_len) * 100
-                print_c(" | ".join((f"{perc:3.0f}%", file_name,)), "new", 2, end="\r")
-            print(end="\n")
+                print_c(" | ".join((f"{perc:3.0f}%", file_name,)), "new", 2, end = "\r")
+            print(end = "\n")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Download files from Canvas")
-    parser.add_argument("token", metavar="TOKEN", help="Canvas access token")
-    parser.add_argument("domain", metavar="DOMAIN", help="Canvas domain")
-
+    parser = argparse.ArgumentParser(description = "Download files from Canvas")
+    parser.add_argument("token", metavar = "TOKEN", help = "Canvas access token")
+    parser.add_argument("domain", metavar = "DOMAIN", help = "Canvas domain")
+    
     parser.add_argument(
         "-f",
-        metavar="FROM",
-        help="Download from modules, folders or both (Default: both)",
-        choices=("modules", "folders", "both"),
-        default="both"
+        metavar = "FROM",
+        help = "Download from modules, folders or both (Default: both)",
+        choices = ("modules", "folders", "both"),
+        default = "both"
     )
-
+    
     parser.add_argument(
         "-o",
-        type=str,
-        metavar="OUT",
-        help="Out directory (Default: CanvasFiles)",
-        default="CanvasFiles"
+        type = str,
+        metavar = "OUT",
+        help = "Out directory (Default: CanvasFiles)",
+        default = "CanvasFiles"
     )
-
+    
     parser.add_argument(
-        "--all", action="store_true", help="Get all courses instead of only favorites"
+        "--all", action = "store_true", help = "Get all courses instead of only favorites"
     )
-
+    
     args = parser.parse_args()
-
+    
     API = CanvasDownloader(args.domain, args.token, args.o)
     API.download_files(args.all, args.f)
